8cf098d6d1f24dbcc6a951e2203b82e0ff38d1be
antirez <antirez@gmail.com>
2015-01-19 18:04:07 +0100

    Test: join test should avoid counting handshaking nodes.

--- tests/cluster/tests/includes/init-tests.tcl
+++ tests/cluster/tests/includes/init-tests.tcl
@@ -49,7 +49,7 @@ test "Cluster Join and auto-discovery test" {
 
     foreach_disque_id id {
         wait_for_condition {
-            [llength [get_cluster_nodes $id]] == [llength $ids]
+            [CI $id cluster_size] == [llength $ids]
         } else {
             fail "Cluster failed to join into a full mesh."
         }

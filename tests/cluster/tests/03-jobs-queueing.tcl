2e7b12ce9d09c1ca81e00317502915c63d10f65f
antirez <antirez@gmail.com>
2015-04-13 12:41:21 +0200

    Test: new test unit for federation. Just the basics for now.

--- tests/cluster/tests/03-jobs-queueing.tcl
+++ tests/cluster/tests/03-jobs-queueing.tcl
@@ -1,7 +1,7 @@
 source "../tests/includes/init-tests.tcl"
 source "../tests/includes/job-utils.tcl"
 
-test "ADDJOB will initiall only queue the job in the target node" {
+test "ADDJOB will initiall queue the job in the target node" {
     set id [D 0 addjob myqueue myjob 5000 replicate 3]
     set job [D 0 show $id]
     assert {$id ne {}}
---
+++ tests/cluster/tests/06-federation.tcl
@@ -0,0 +1,27 @@
+source "../tests/includes/init-tests.tcl"
+source "../tests/includes/job-utils.tcl"
+
+set numjobs 100
+
+test "Add multiple jobs to the same queue, different nodes" {
+    for {set j 0} {$j < $numjobs} {incr j} {
+        set i [randomInt $::instances_count]
+        set id [D $i addjob myqueue job_$j 5000 replicate 3]
+        set job [D $i show $id]
+        assert {$id ne {}}
+        assert {[count_job_copies $job active] == 2}
+    }
+}
+
+test "Make sure node 0 does not have all the jobs queued" {
+    assert {[D 0 qlen myqueue] != $numjobs}
+}
+
+test "I should be able to fetch each job from instance 0" {
+    set jobs {}
+    while {[llength [lsort -unique $jobs]] != $numjobs} {
+        set myjob [D 0 getjob from myqueue TIMEOUT 5000]
+        lappend jobs [lindex $myjob 0 2]
+        D 0 ackjob [lindex $myjob 0 1]
+    }
+}

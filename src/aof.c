hanzoai/redis

bugfix: errno might change before logging
Signed-off-by: Yongyue Sun <abioy.sun@gmail.com>

src/aof.c
@@ -1057,9 +1057,9 @@ int rewriteAppendOnlyFile(char *filename) {
     return REDIS_OK;
 
 werr:
+    redisLog(REDIS_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     fclose(fp);
     unlink(tmpfile);
-    redisLog(REDIS_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     if (di) dictReleaseIterator(di);
     return REDIS_ERR;
 }

antirez/disque

src/aof.c
@@ -856,9 +856,9 @@ flush_and_rename:
     return DISQUE_OK;
 
 werr:
+    serverLog(DISQUE_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     fclose(fp);
     unlink(tmpfile);
-    serverLog(DISQUE_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     if (di) dictReleaseIterator(di);
     return DISQUE_ERR;
 }